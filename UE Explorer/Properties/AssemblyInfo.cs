f2e4aeb635d201d472cb38c728828fac01bafd4d
EliotVU <eliot_van@hotmail.com>
2012-11-01 21:45:06 +0100

    [Fix]: Correct version and copyright.

--- UE Explorer/Properties/AssemblyInfo.cs
+++ UE Explorer/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("UE Explorer")]
-[assembly: AssemblyCopyright("Copyright 2012 Eliot van Uytfanghe.")]
+[assembly: AssemblyCopyright("© 2012 Eliot van Uytfanghe. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.1.0" )]
-[assembly: AssemblyFileVersion( "1.1.0" )]
+[assembly: AssemblyVersion( "1.2.0" )]
+[assembly: AssemblyFileVersion( "1.2.0" )]
