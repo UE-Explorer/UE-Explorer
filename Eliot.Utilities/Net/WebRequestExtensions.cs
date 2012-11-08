1eaed28bf56307634f0824d19c21c7aa7458d070
EliotVU <eliot_van@hotmail.com>
2012-11-08 05:02:47 +0100

    [Add]: Post and a Get method to WebRequest

---
+++ Eliot.Utilities/Net/WebRequestExtensions.cs
@@ -0,0 +1,62 @@
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Eliot.Utilities.Net
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public static class WebRequestExtensions
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="webReq"></param>
+		/// <param name="data"></param>
+		/// <returns></returns>
+		public static string Post( this WebRequest webReq, string data )
+		{
+			var buffer = Encoding.UTF8.GetBytes( data );
+			webReq.Proxy = WebRequest.DefaultWebProxy;
+			webReq.Method = "POST";
+			webReq.ContentType = "application/x-www-form-urlencoded";
+			webReq.ContentLength = buffer.Length;
+
+			using( var postStream = webReq.GetRequestStream())
+			{
+				postStream.Write( buffer, 0, buffer.Length );
+				
+			}	
+
+			string result;
+			using( var response = webReq.GetResponse() )
+			{ 
+				using( var responseReader = new StreamReader( response.GetResponseStream() ) )
+				{
+					result = responseReader.ReadToEnd();
+				}
+			}
+			return result;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="webReq"></param>
+		/// <returns></returns>
+		public static MemoryStream Get( this WebRequest webReq )
+		{
+			webReq.Proxy = WebRequest.DefaultWebProxy;
+			var buffer = new MemoryStream();
+			using( var response = webReq.GetResponse() )
+			{
+				using( var responseStream = response.GetResponseStream() )
+				{
+					responseStream.CopyTo( buffer );
+				}
+			}
+			return buffer;
+		}
+	}
+}
--- Eliot.Utilities/StringExtensions.cs
+++ Eliot.Utilities/StringExtensions.cs
@@ -2,28 +2,60 @@
 
 namespace Eliot.Utilities
 {
+	/// <summary>
+	/// 
+	/// </summary>
 	public static class StringExtensions
 	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="s"></param>
+		/// <param name="index"></param>
+		/// <returns>Left side from index.</returns>
 		public static string Left( this string s, int index )
 		{
 			return s.Substring( 0, index );
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="s"></param>
+		/// <param name="index"></param>
+		/// <returns>Mid side from index.</returns>
 		public static string Mid( this string s, int index )
 		{
 			return s.Substring( index, s.Length - index );
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="s"></param>
+		/// <param name="index"></param>
+		/// <returns></returns>
 		public static string Right( this string s, int index )
 		{
 			return s.Substring( 0, s.Length - index );
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="s"></param>
+		/// <param name="i"></param>
+		/// <returns></returns>
 		public static char FromInt( this Char s, int i )
 		{
 			return (Char)i;
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="s"></param>
+		/// <returns></returns>
 		public static int FromChar( this Char s )
 		{
 			return s;
