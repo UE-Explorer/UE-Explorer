8f1f1ca980879b9d0d9755216eda9ac91575190f
EliotVU <eliot_van@hotmail.com>
2013-01-05 03:12:00 +0100

    [Add]: DrawingExtensions to Darken or Lighten any color.

---
+++ Eliot.Utilities/DrawingExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+
+namespace Eliot.Utilities
+{
+    public static class DrawingExtensions
+    {
+        public static Color Darken( this Color c, float pct )
+        {
+            pct = 1.0F - pct/100F;
+            var r = (byte)(c.R*pct);
+            var g = (byte)(c.G*pct);
+            var b = (byte)(c.B*pct);
+            return Color.FromArgb( c.A, r, g, b );
+        }
+
+        public static Color Lighten( this Color c, float pct )
+        {
+            pct = 1.0F + pct/100F;
+            var r = (byte)(c.R*pct);
+            var g = (byte)(c.G*pct);
+            var b = (byte)(c.B*pct);
+            return Color.FromArgb( c.A, r, g, b );
+        }
+    }
+}
