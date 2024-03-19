53698dd51068a52a1eba367b7d277dc60f816ccc
Eliot <contact@eliotvu.com>
2024-03-19 16:14:45 +0100

    Upgrade project to .NET SDK

--- UE Explorer/Properties/AssemblyInfo.cs
+++ UE Explorer/Properties/AssemblyInfo.cs
@@ -1,18 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Resources;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("UE Explorer")]
-[assembly: AssemblyDescription("An UnrealScript decompiler for Unreal Engine 1, 2, and 3. Supporting all Epic Games for the PC such as Unreal, UT, UT2004, UT3, and the Unreal Development Kit(UDK).")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("EliotVU")]
-[assembly: AssemblyProduct("UE Explorer")]
-[assembly: AssemblyCopyright("© 2009-2023 Eliot van Uytfanghe. All rights reserved.")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+﻿using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -22,16 +8,3 @@ using System.Resources;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5fe16f07-b456-4f46-8d4a-1e375478a958")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.2")]
-[assembly: AssemblyFileVersion("1.3.2")]
-[assembly: NeutralResourcesLanguage("en")]
