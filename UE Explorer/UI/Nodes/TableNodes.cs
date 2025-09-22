4c008c95655e0e306acab052eda7b3823cb9f5ef
Eliot <contact@eliotvu.com>
2025-09-22 22:10:46 +0200

    Display the normalized 'Object Flags' in the export table nodes.

--- UE Explorer/UI/Nodes/TableNodes.cs
+++ UE Explorer/UI/Nodes/TableNodes.cs
@@ -21,7 +21,7 @@ namespace UEExplorer.UI.Nodes
 
     public sealed class UExportNode : UTableNode
     {
-        public new UExportTableItem Table{ private get{ return base.Table as UExportTableItem; } set{ base.Table = value; } }
+        public new UExportTableItem Table{ private get{ return (UExportTableItem)base.Table; } set{ base.Table = value; } }
 
         public UExportNode()
         {
@@ -43,72 +43,69 @@ namespace UEExplorer.UI.Nodes
 
             Nodes.Clear();
             ulong objFlags = Table.ObjectFlags;
-                if( objFlags != 0 )
-                {
-                    string flagTitle = "Flags(" + UnrealMethods.FlagToString( Table.ObjectFlags ) + ")";
-                    var flagNode = Nodes.Add( flagTitle  );
-                    flagNode.ToolTipText = UnrealMethods.FlagsListToString
-                    ( 
-                        UnrealMethods.FlagsToList( typeof(ObjectFlagsLO), typeof(ObjectFlagsHO), Table.ObjectFlags ) 
-                    );	
-                }
+            if( objFlags != 0 )
+            {
+                string flagTitle = $"ObjectFlags:{Table.ObjectFlags:X8}";
+                var flagNode = Nodes.Add( flagTitle  );
+                flagNode.ToolTipText = new UnrealFlags<ObjectFlag>(Table.ObjectFlags, Table.Owner.Branch.EnumFlagsMap[typeof(ObjectFlag)]).ToString();
+            }
 
-                if( Table.ExportFlags != 0 )
-                {
-                    Nodes.Add( "Export Flags:" + UnrealMethods.FlagToString( Table.ExportFlags ) );
-                }
+            if( Table.ExportFlags != 0 )
+            {
+                Nodes.Add( "Export Flags:" + UnrealMethods.FlagToString( Table.ExportFlags ) );
+            }
 
-                Nodes.Add( "Object:" + Table );
-                if( Table.ClassIndex != 0 )
-                {
-                    Nodes.Add( "Class:" + Table.ClassTable );
-                }
+            Nodes.Add( "Object:" + Table );
+            if( Table.ClassIndex != 0 )
+            {
+                Nodes.Add( "Class:" + Table.Class );
+            }
 
-                if( Table.SuperIndex != 0 )
-                {
-                    Nodes.Add( "Super:" + Table.SuperTable);
-                }
+            if( Table.SuperIndex != 0 )
+            {
+                Nodes.Add( "Super:" + Table.Super);
+            }
 
-                if( Table.OuterIndex != 0 )
-                {
-                    Nodes.Add( "Outer:" + Table.OuterTable );
-                }
+            if( Table.OuterIndex != 0 )
+            {
+                Nodes.Add( "Outer:" + Table.Outer );
+            }
 
-                if( Table.ArchetypeIndex != 0 )
-                {
-                    Nodes.Add( "Archetype:" + Table.ArchetypeTable );
-                }
+            if( Table.ArchetypeIndex != 0 )
+            {
+                Nodes.Add( "Archetype:" + Table.Archetype );
+            }
 
-                if (Table.ComponentMap != null && Table.ComponentMap.Count > 0)
+            if (Table.ComponentMap != null && Table.ComponentMap.Count > 0)
+            {
+                var componentsNode = Nodes.Add("Components");
+                foreach (var keyValuePair in Table.ComponentMap)
                 {
-                    var componentsNode = Nodes.Add("Components");
-                    foreach (var keyValuePair in Table.ComponentMap)
+                    var obj = Table.Owner.IndexToObject(keyValuePair.Value);
+                    if (obj != null)
                     {
-                        var obj = Table.Owner.IndexToObject(keyValuePair.Value);
-                        if (obj != null)
-                        {
-                            componentsNode.Nodes.Add(obj.ToString());
-                        }
+                        componentsNode.Nodes.Add(obj.ToString());
                     }
                 }
+            }
                 
-                if (Table.PackageFlags != 0)
-                {
-                    Nodes.Add("PackageFlags:" + Table.PackageFlags);
-                }
+            if (Table.PackageFlags != 0)
+            {
+                Nodes.Add("PackageFlags:" + Table.PackageFlags);
+            }
                 
-                if ((Guid)Table.PackageGuid != Guid.Empty)
-                {
-                    Nodes.Add("PackageGuid:" + Table.PackageGuid);
-                }
+            if ((Guid)Table.PackageGuid != Guid.Empty)
+            {
+                Nodes.Add("PackageGuid:" + Table.PackageGuid);
+            }
 
-                if ( Table.SerialSize > 0 )
-                {
-                    Nodes.Add( "Object Size:" + Table.SerialSize );
-                    Nodes.Add( "Object Offset:" + Table.SerialOffset );
-                }
+            if ( Table.SerialSize > 0 )
+            {
+                Nodes.Add( "Object Size:" + Table.SerialSize );
+                Nodes.Add( "Object Offset:" + Table.SerialOffset );
+            }
 
-                _IsInitialized = true;
+            _IsInitialized = true;
         }
 
         public override void Expanded()
@@ -124,7 +121,7 @@ namespace UEExplorer.UI.Nodes
 
     public sealed class UImportNode : UTableNode
     {
-        public new UImportTableItem Table{ private get{ return base.Table as UImportTableItem; } set{ base.Table = value; } }
+        public new UImportTableItem Table{ private get{ return (UImportTableItem)base.Table; } set{ base.Table = value; } }
 
         public UImportNode()
         {
@@ -146,14 +143,14 @@ namespace UEExplorer.UI.Nodes
 
             Nodes.Clear();
             Nodes.Add( "Object:" + Table );
-                Nodes.Add( "Class:" + Table.ClassName + "(" + Table.ClassIndex + ")" );
-                Nodes.Add( "Package:" + Table.PackageName + "(" + (int)Table.PackageName + ")" );
-                if( Table.OuterIndex != 0 )
-                {
-                    Nodes.Add( "Outer:" + Table.OuterTable );
-                }
+            Nodes.Add( "Class:" + Table.ClassName );
+            Nodes.Add( "Package:" + Table.ClassPackageName );
+            if( Table.OuterIndex != 0 )
+            {
+                Nodes.Add( "Outer:" + Table.Outer );
+            }
 
-                _IsInitialized = true;
+            _IsInitialized = true;
         }
 
         public override void Expanded()
